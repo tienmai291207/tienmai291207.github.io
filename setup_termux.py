31a1d8741d353700e936295336eba3587658f232
tienmai291207 <106916721+tienmai291207@users.noreply.github.com>
2024-07-07 22:41:42 +0700

    Update setup_termux.py

--- setup_termux.py
+++ setup_termux.py
@@ -6,23 +6,26 @@ def mtt_setup(command):
     print(f"Đang setup: {command}")
     try:
         subprocess.run(command, shell=True, check=True)
-    except subprocess.CalledProcessError as e:
-        print("Lỗi rồi")
+    except subprocess.CalledProcessError:
+        pass  # Skip the error and continue with the next command
 
 commands = [
-    "apt update -y -y -y -y -y -y",
+    "apt update -y",
     "apt upgrade -y",
     "pkg install php -y",
     "pkg install python -y",
     "pkg install git -y",
     "pkg install wget -y",
+    "pkg install termux-api -y",  # Install Termux API
     "pip install --upgrade pip",
     "pip install forbidden",
     "pip install requests",
     "pip install pycurl",
     "pip install bs4",
     "pip install colorama",
-    "pip install pystyle"
+    "pip install pystyle",
+    "pip install pyTelegramBotAPI",  # telebot module
+    "pip install aiohttp"  # common module for asynchronous HTTP requests
 ]
 
 for cmd in commands:
@@ -30,3 +33,4 @@ for cmd in commands:
 
 os.system("clear")
 print("\033[32mSetup successfully\nGroup 1: https://t.me/nolow_2k7\nGroup 2: https://t.me/pulfsharemodchat\nCreated by ᴍʀ ᴄs 🌷")
+subprocess.run(["termux-open-url", "https://t.me/nolow_2k7"])  # Open URL using Termux API
