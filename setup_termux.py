b79e690f4303333ec1a630f3e0a9279b2a267789
tienmai291207 <106916721+tienmai291207@users.noreply.github.com>
2024-07-07 21:56:58 +0700

    Update setup_termux.py

--- setup_termux.py
+++ setup_termux.py
@@ -1,14 +1,15 @@
 import subprocess
 import os
+
 def mtt_setup(command):
+    os.system("clear")
+    print(f"Đang setup: {command}")
     try:
         subprocess.run(command, shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     except subprocess.CalledProcessError as e:
-        print(f"Lỗi rồi")
-        
+        print("Lỗi rồi")
+
 commands = [
-    "pkg install termux-am -y",
-    "termux-setup-storage",
     "apt update -y && apt upgrade -y",
     "pkg install php -y",
     "pkg install python -y",
@@ -22,5 +23,6 @@ commands = [
 
 for cmd in commands:
     mtt_setup(cmd)
+
 os.system("clear")
-print("\033[32mSetup successfully\nGroup 1: https://t.me/nolow_2k7/nGroup 2: https://t.me/pulfsharemodchat/nCreated by ᴍʀ ᴄs 🌷")
+print("\033[32mSetup successfully\nGroup 1: https://t.me/nolow_2k7\nGroup 2: https://t.me/pulfsharemodchat\nCreated by ᴍʀ ᴄs 🌷")
