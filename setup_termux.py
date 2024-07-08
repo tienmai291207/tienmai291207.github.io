b9c5918388128555c77e9c577410527fc711edb2
tienmai291207 <106916721+tienmai291207@users.noreply.github.com>
2024-07-08 09:05:30 +0700

    Update setup_termux.py

--- setup_termux.py
+++ setup_termux.py
@@ -1,6 +1,13 @@
 import os
+os.system('clear')
+print("\033[93mĐang setup: pkg install termux-am -y\033[0m")
 os.system('pkg install termux-am -y')
+print("\033[93mĐang setup: termux-setup-storage\033[0m")
 os.system('termux-setup-storage')
+print("\033[93mĐang setup: apt update -y\033[0m")
+os.system('apt update -y')
+print("\033[93mĐang setup: apt upgrade -y\033[0m")
+os.system('apt upgrade -y')
 os.system('pip install requests')
 import requests 
 try:
